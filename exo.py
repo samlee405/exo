ad1f075fb252d7629b6c8188e32d4cc7dbabd509
Sam <samlee405@yahoo.com>
2020-07-28 21:45:25 -0700

    finishes implementation

--- exo.py
+++ exo.py
@@ -3,7 +3,11 @@ import pyscreenshot
 from PIL import Image
 import pytesseract
 import cv2
-import re
+import pyautogui
+from pynput import keyboard
+import json
+import time
+import threading
 import timeit
 
 
@@ -11,54 +15,12 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Sam\AppData\Local\Tesseract-O
 root_dir = os.path.dirname(os.path.abspath(__file__))
 screen_shot_path = os.path.join(root_dir, "images", "screenshot.png")
 
-stats = [
-    "Vitality",
-    "AP",
-    "MP",
-    "Initiative",
-    "Prospecting",
-    "Range",
-    "Summons",
-    "Wisdom",
-    "Strength",
-    "Intelligence",
-    "Chance",
-    "Agility",
-    "AP Parry",
-    "AP Reduction",
-    "MP Parry",
-    "MP Reduction",
-    "Critical",
-    "Heals",
-    "Lock",
-    "Dodge",
-    "Power",
-    "Damage",
-    "Critical Damage",
-    "Neutral Damage",
-    "Earth Damage",
-    "Fire Damage",
-    "Water Damage",
-    "Air Damage",
-    "Reflect",
-    "Trap Damage",
-    "Power \(traps\)",
-    "Pushback Damage",
-    "Spell Damage",
-    "Weapon Damage",
-    "Ranged Damage",
-    "Melee Damage",
-    "Neutral Resistance",
-    "Earth Resistance",
-    "Fire Resistance",
-    "Water Resistance",
-    "Air Resistance",
-    "Critical Resistance",
-    "Pushback Resistance",
-    "Ranged Resistance",
-    "Melee Resistance",
-    "pods",
+items = [
+    "warGauntlet"
 ]
+
+should_stop_maging = False
+is_running = False
 
 
 def take_screen_shot():
@@ -97,24 +59,72 @@ def get_stats_from_text(raw_text):
         if "%" in value:
             value = value.replace("%", "")
             stat = "% " + stat
-        stats[stat] = value
-
-        print((stat, value))
+        stats[stat] = int(value)
 
     return stats
 
 
-def exo():
-    start = timeit.default_timer()
+def mage(stats, item):
+    with open("items.json", "r") as file:
+        data = json.load(file)
+        item_data = data[item]
 
-    take_screen_shot()
-    image_preprocessing()
-    raw_text = get_text_from_image()
-    stats = get_stats_from_text(raw_text)
+        is_done = True
 
-    stop = timeit.default_timer()
-    print('\nTime: ', stop - start)
+        for stat_data in item_data["stats"]:
+            stat = stat_data["stat"]
+            if stats[stat] <= stat_data["threshold"]:
+                pyautogui.moveTo(stat_data["position"]["x"], stat_data["position"]["y"], 0.1)
+                pyautogui.doubleClick()
+                pyautogui.moveTo(1010, 240, 0.2)
+                pyautogui.dragTo(1010, 240, button = 'left')
+                pyautogui.click(1010, 240)
+                is_done = False
+                break
+
+        return is_done
+
+
+def run_maging_script(item):
+    is_done = False
+    while not is_done and not should_stop_maging:
+        take_screen_shot()
+        image_preprocessing()
+        raw_text = get_text_from_image()
+        stats = get_stats_from_text(raw_text)
+        is_done = mage(stats, item)
+
+        time.sleep(0.5)
+
+    if is_done:
+        print("Maging finished")
+
+
+def main(item):
+    def on_press(key):
+        global should_stop_maging
+        global is_running
+        if key == keyboard.Key.f11 and not is_running:
+            print("Starting mage")
+            should_stop_maging = False
+            is_running = True
+            script_thread = threading.Thread(target=run_maging_script, args=(item, ))
+            script_thread.start()
+        elif key == keyboard.Key.f12 and is_running:
+            print("Early exit detected, stopping mage")
+            should_stop_maging = True
+            is_running = False
+        elif key == keyboard.Key.end:
+            print("Stopping script")
+            return False
+
+    print("Please bring up your game window with your item ready to begin maging. "
+        "When ready, press F11 to start maging. If at any time you'd like to "
+        "pause execution, press F12. To exit the script altogether, press End")
+
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
 
 
 if __name__ == "__main__":
-    exo()
+    main("warGauntlet")
